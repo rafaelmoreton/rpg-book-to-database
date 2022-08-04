894dbb20d6082bc848a0a31854c561bf1250a711
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2022-08-04 09:55:53 -0300

    Add text parser to find item entries

--- parser.rb
+++ parser.rb
@@ -1,6 +1,31 @@
 file = open("cap-5-itens.txt", "r")
-contents = file.read
-excerpt_start = contents.index("ARMADURAS LEVES")
-excerpt_end = contents.index("[[armor-end]]")
+contents = file.readlines
+excerpt_start = contents.index { |line| line.include?("ARMADURAS LEVES") }
+excerpt_end = contents.index { |line| line.include?("[[armor-end]]") }
 excerpt = contents[excerpt_start...excerpt_end]
-puts excerpt
+
+# Remove meaningless line breaks
+test = []
+excerpt.each do |line|
+  if line.length > 40 &&
+     line.chomp[-1] != "."
+    test << line.delete("\n")
+  else
+    test << line
+  end
+end
+
+# Find which paragraphs contain items
+items = []
+test.join.split("\n").each do |paragraph|
+  if paragraph[0...23].include?(".")
+    items << paragraph + "\n\n"
+  end
+end
+
+puts items
+
+# puts test.join
+# parsed_file = open("parsed-file.txt", "w")
+# parsed_file.puts test.join
+# parsed_file.close
